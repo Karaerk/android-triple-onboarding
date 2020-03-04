2e16bdd5bdffd777923b91b48260e82c02ebfa21
K.Karaer <kerim.karaer@hva.nl>
2020-03-04 09:46:29 +0100

    Finish setting up Firebase

--- app/src/main/java/com/wearetriple/tripleonboarding/MainActivity.kt
+++ app/src/main/java/com/wearetriple/tripleonboarding/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.wearetriple.tripleonboarding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("something")
+        myRef.setValue("Hello World!! It works :)")
+
+//        val database = FirebaseDatabase.getInstance()
+//        val myRef = database.getReference("message")
+//
+//        myRef.setValue("Hello, World!")
+//
+//        FirebaseDatabase.getInstance().getReference().child("users").setValue("value")
     }
 }
