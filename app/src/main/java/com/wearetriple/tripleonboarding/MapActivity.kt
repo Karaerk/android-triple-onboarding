bd92e39c33271ee3e1a8832bbcb36728855cad7b
K.Karaer <kerim.karaer@hva.nl>
2020-03-10 14:57:20 +0100

    Add pinch support to ImageView

--- app/src/main/java/com/wearetriple/tripleonboarding/MapActivity.kt
+++ app/src/main/java/com/wearetriple/tripleonboarding/MapActivity.kt
@@ -1,12 +1,36 @@
 package com.wearetriple.tripleonboarding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_map.*
+
 
 class MapActivity : AppCompatActivity() {
+
+    private var mScaleGestureDetector: ScaleGestureDetector? = null
+    private var mScaleFactor = 1.0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        mScaleGestureDetector = ScaleGestureDetector(this, ScaleListener())
+    }
+
+    override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
+        mScaleGestureDetector?.onTouchEvent(motionEvent)
+        return true
+    }
+
+    private inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(scaleGestureDetector: ScaleGestureDetector): Boolean {
+            mScaleFactor *= scaleGestureDetector.scaleFactor
+            mScaleFactor = Math.max(1f, Math.min(mScaleFactor, 5.0f))
+            ivMap?.setScaleX(mScaleFactor)
+            ivMap?.setScaleY(mScaleFactor)
+            return true
+        }
     }
 }
