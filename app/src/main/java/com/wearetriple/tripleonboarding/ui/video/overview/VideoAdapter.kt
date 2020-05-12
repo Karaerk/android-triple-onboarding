e87139a9918f9b4b0ed53d850758c880af8b134f
K.Karaer <kerim.karaer@hva.nl>
2020-05-12 11:27:54 +0200

    Generate video thumbnail when there's no custom thumbnail available

--- app/src/main/java/com/wearetriple/tripleonboarding/ui/video/overview/VideoAdapter.kt
+++ app/src/main/java/com/wearetriple/tripleonboarding/ui/video/overview/VideoAdapter.kt
@@ -14,6 +14,7 @@ import com.wearetriple.tripleonboarding.model.Video
 import com.wearetriple.tripleonboarding.ui.helper.AbstractAdapter
 import kotlinx.android.synthetic.main.item_video.view.*
 
+
 /**
  * Used to prepare recyclerview's items.
  */
@@ -26,25 +27,53 @@ class VideoAdapter(
      * Prepares the view before passing it to the RecyclerView.
      */
     inner class ViewHolder(itemView: View) : AbstractAdapter<Video>.ViewHolder(itemView) {
+        private val roundingRadius = 5
+        private val thumbnailSize = 0.25f
+
+        private val requestOptions = fitCenterTransform()
+            .transform(RoundedCorners(roundingRadius))
+            .error(ColorDrawable(Color.WHITE))
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .override(
+                itemView.ivVideoThumbnail.width,
+                itemView.ivVideoThumbnail.height
+            )
 
         override fun bind(item: Video) {
             itemView.tvTitle.text = item.title
             itemView.cvVideo.setOnClickListener { clickListener(item) }
 
-            val roundingRadius = 5
-            val thumbnailSize = 0.25f
-            val reqOpt = fitCenterTransform()
-                .transform(RoundedCorners(roundingRadius))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .override(
-                    itemView.ivVideoThumbnail.width,
-                    itemView.ivVideoThumbnail.height
-                )
+            prepareThumbnail(item)
+        }
 
+        /**
+         * Prepares the video's thumbnail by generating a thumbnail out of the video whenever
+         * there's no custom thumbnail available in [item]
+         */
+        private fun prepareThumbnail(item: Video) {
+
+            if (item.thumbnail.isNotEmpty()) {
+                Glide.with(itemView.context)
+                    .load(item.thumbnail)
+                    .thumbnail(thumbnailSize)
+                    .apply(requestOptions)
+                    .placeholder(ColorDrawable(Color.WHITE))
+                    .into(itemView.ivVideoThumbnail)
+            } else {
+                generateThumbnail(item)
+            }
+        }
+
+        /**
+         * Picks up a frame of the item's linked video and uses that as a thumbnail in case the
+         * item's thumbnail attribute is empty.
+         */
+        private fun generateThumbnail(item: Video) {
             Glide.with(itemView.context)
-                .load(item.thumbnail)
+                .asBitmap()
+                .load(item.url)
                 .thumbnail(thumbnailSize)
-                .apply(reqOpt)
+                .apply(requestOptions)
                 .placeholder(ColorDrawable(Color.WHITE))
                 .into(itemView.ivVideoThumbnail)
         }
